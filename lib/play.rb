93afef6dd23910211a6f43530fed6efdc3d2019b
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-11-20 22:33:17 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -39,9 +39,9 @@ end
 
 def play(board)
 counter = 0
- until counter == 9
-  turn(board)
+ #until counter == 9
+  #turn(board)
   #puts "hi #{counter}"
-  counter += 1
-  end
+  #counter += 1
+  #end
 end
